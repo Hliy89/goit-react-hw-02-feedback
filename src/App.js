cbc6cc781cdc62e2ba866e29b6ff3659659ffb62
Yaroslav Hliy <hliy.yaroslav@gmail.com>
2021-04-22 15:14:25 +0300

    homework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import style from "./App.module.css";
+
+import Feedback from "./components/Feedback";
+
+const App = () => {
+    return (
+      <div className={style.container}>
+        <Feedback />
+      </div>
+    )
+};
 
 export default App;
